7b9e1084b34fc2ea81e678c9b5acb37566211105
Alex Turyev <AATuryev@sberbank.ru>
2022-01-20 16:28:25 +0300

    init

---
+++ precache-manifest.2ab09085fe873b642922bc16f09bcac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5946ef1c077e23daa2693b989e648f05",
+    "url": "/test_canvas_app/index.html"
+  },
+  {
+    "revision": "e783106a05bf6cf1e2f9",
+    "url": "/test_canvas_app/static/css/main.43c1b5f0.chunk.css"
+  },
+  {
+    "revision": "dcc4289bfdb005b04f50",
+    "url": "/test_canvas_app/static/js/2.fd50676c.chunk.js"
+  },
+  {
+    "revision": "29bf219c3a6fdf70d818",
+    "url": "/test_canvas_app/static/js/3.038ade9f.chunk.js"
+  },
+  {
+    "revision": "e783106a05bf6cf1e2f9",
+    "url": "/test_canvas_app/static/js/main.200540e0.chunk.js"
+  },
+  {
+    "revision": "b21443ce59fed931ad81",
+    "url": "/test_canvas_app/static/js/runtime~main.89586988.js"
+  }
+]);
